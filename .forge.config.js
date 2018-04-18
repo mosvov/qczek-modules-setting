627e704d019e5f4833465e30224ebeec22405adf
Volodymyr Moskalyk <mosvov@gmail.com>
2018-04-18 12:36:12 +0200

    Added travis.yml

---
+++ .forge.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "make_targets": {
+        "win32": [
+            "squirrel"
+        ],
+        "darwin": [
+            "zip"
+        ],
+        "linux": [
+            "deb",
+            "rpm"
+        ]
+    },
+    "electronPackagerConfig": {
+        "packageManager": "yarn"
+    },
+    "electronWinstallerConfig": {
+        "name": "ebyte_modules_setting"
+    },
+    "electronInstallerDebian": {},
+    "electronInstallerRedhat": {},
+    "github_repository": {
+        "owner": "mosvov",
+        "name": "ebyte-modules-setting"
+    },
+    "windowsStoreConfig": {
+        "packageName": "",
+        "name": "ebytemodulessetting"
+    }
+}
