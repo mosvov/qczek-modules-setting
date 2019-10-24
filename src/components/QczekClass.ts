1c16533d60f322611490ddee5dbdefb1f1bbcaf0
mosvov <vmoskalyk@goodrx.com>
2019-10-24 15:23:08 -0700

    Added tslint instead of eslint and prettier

--- src/components/QczekClass.ts
+++ src/components/QczekClass.ts
@@ -1,236 +1,236 @@
 import * as SerialPort from 'serialport';
 
 export interface IModuleParams {
-    version?: string;
-    features?: string;
-    isMaster: boolean;
-    pCode: number;
-    noOfCPPMChnls: number;
-    pwrSelChnl?: number;
-    txMinPwr: number;
-    txMidPwr: number;
-    txMaxPwr: number;
-    serPortSpd: number;
-    serMode: number;
-    fr0: number;
-    fr1: number;
-    fr2: number;
-    fr3: number;
-    fr4: number;
-    fr5: number;
-    fr6: number;
-    fr7: number;
-    beaFreq: number;
-    proto: number;
-    devThrs: number;
-    sTelFrmR: number;
-    lTelFrmR: number;
-    racem: number;
-    rssiChnl: number;
-    rssiT: number;
-    timeToFS?: number;
-    wdTime?: number;
-    fsDontT?: number;
-    lbeep?: number;
-    m0PWMChn?: number;
-    m1PWMChn?: number;
-    silent?: number;
-    fs00?: number;
-    fs01?: number;
-    fs02?: number;
-    fs03?: number;
-    fs04?: number;
-    fs05?: number;
-    fs06?: number;
-    fs07?: number;
-    fs08?: number;
-    fs09?: number;
-    fs010?: number;
-    fs011?: number;
-    fs012?: number;
-    fs013?: number;
-    fs014?: number;
-    fs015?: number;
+  version?: string;
+  features?: string;
+  isMaster: boolean;
+  pCode: number;
+  noOfCPPMChnls: number;
+  pwrSelChnl?: number;
+  txMinPwr: number;
+  txMidPwr: number;
+  txMaxPwr: number;
+  serPortSpd: number;
+  serMode: number;
+  fr0: number;
+  fr1: number;
+  fr2: number;
+  fr3: number;
+  fr4: number;
+  fr5: number;
+  fr6: number;
+  fr7: number;
+  beaFreq: number;
+  proto: number;
+  devThrs: number;
+  sTelFrmR: number;
+  lTelFrmR: number;
+  racem: number;
+  rssiChnl: number;
+  rssiT: number;
+  timeToFS?: number;
+  wdTime?: number;
+  fsDontT?: number;
+  lbeep?: number;
+  m0PWMChn?: number;
+  m1PWMChn?: number;
+  silent?: number;
+  fs00?: number;
+  fs01?: number;
+  fs02?: number;
+  fs03?: number;
+  fs04?: number;
+  fs05?: number;
+  fs06?: number;
+  fs07?: number;
+  fs08?: number;
+  fs09?: number;
+  fs010?: number;
+  fs011?: number;
+  fs012?: number;
+  fs013?: number;
+  fs014?: number;
+  fs015?: number;
 }
 
 export const DEFAULT_MODULE_PARAMS: IModuleParams = {
-    isMaster: true,
-    pCode: 0,
-    noOfCPPMChnls: 16,
-    pwrSelChnl: 0,
-    rssiChnl: 0,
-    proto: 0,
-    serPortSpd: 57600,
-    serMode: 0,
-    rssiT: 0,
-    txMinPwr: 0,
-    txMidPwr: 4,
-    txMaxPwr: 15,
-    devThrs: 20,
-    sTelFrmR: 1,
-    lTelFrmR: 6,
-    racem: 0,
-    beaFreq: 0,
-    fr0: 0,
-    fr1: 0,
-    fr2: 0,
-    fr3: 0,
-    fr4: 0,
-    fr5: 0,
-    fr6: 0,
-    fr7: 0,
+  isMaster: true,
+  pCode: 0,
+  noOfCPPMChnls: 16,
+  pwrSelChnl: 0,
+  rssiChnl: 0,
+  proto: 0,
+  serPortSpd: 57600,
+  serMode: 0,
+  rssiT: 0,
+  txMinPwr: 0,
+  txMidPwr: 4,
+  txMaxPwr: 15,
+  devThrs: 20,
+  sTelFrmR: 1,
+  lTelFrmR: 6,
+  racem: 0,
+  beaFreq: 0,
+  fr0: 0,
+  fr1: 0,
+  fr2: 0,
+  fr3: 0,
+  fr4: 0,
+  fr5: 0,
+  fr6: 0,
+  fr7: 0,
 };
 
 export default class QczekClass {
-    port: SerialPort;
-    onError: (errorMessage: string) => void;
-    onParams: (params: Partial<IModuleParams>) => void;
+  port: SerialPort;
+  onError: (errorMessage: string) => void;
+  onParams: (params: Partial<IModuleParams>) => void;
 
-    constructor(port: SerialPort) {
-        this.port = port;
+  constructor(port: SerialPort) {
+    this.port = port;
 
-        this.port.on('open', () => this.readParams());
+    this.port.on('open', () => this.readParams());
 
-        const parser = port.pipe(new SerialPort.parsers.Readline({delimiter: '\n'}));
-        parser.on('data', (data: string) => this.onData(data));
+    const parser = port.pipe(new SerialPort.parsers.Readline({ delimiter: '\n' }));
+    parser.on('data', (data: string) => this.onData(data));
+  }
+
+  readParams() {
+    this.port.write(QczekClass.alignCommand('{ParGetAll}'), this.handleError);
+  }
+
+  saveParams(params: IModuleParams) {
+    if (!params) {
+      return;
     }
 
-    readParams() {
-        this.port.write(QczekClass.alignCommand('{ParGetAll}'), this.handleError);
+    Object.keys(params).forEach(key => {
+      const line = QczekClass.generateParamLine(key, params[key]);
+      if (!line || key === 'version' || key === 'features' || key === 'isMaster') {
+        return;
+      }
+
+      console.log('Write to port', line, line.length);
+
+      this.port.write(QczekClass.alignCommand(line), this.handleError);
+    });
+
+    this.port.write(QczekClass.alignCommand('{ParSave}'), this.handleError);
+  }
+
+  /**
+   * Command that we send to serial should have minimum 10 char
+   * Is because DMA buffer is processed every 10 chars
+   *
+   * If we have 22 char in command, we should send minimum 30
+   */
+  static alignCommand(command: string): string {
+    // @ts-ignore
+    return command.padEnd(30);
+  }
+
+  onData(data: string) {
+    if (!data) {
+      console.error('No data in response');
+      return;
     }
 
-    saveParams(params: IModuleParams) {
-        if (!params) {
-            return;
-        }
+    console.log('data', data);
 
-        Object.keys(params).forEach((key) => {
-            const line = QczekClass.generateParamLine(key, params[key]);
-            if (!line || key === 'version' || key === 'features' || key === 'isMaster') {
-                return;
-            }
-
-            console.log('Write to port', line, line.length);
-
-            this.port.write(QczekClass.alignCommand(line), this.handleError);
-        });
-
-        this.port.write(QczekClass.alignCommand('{ParSave}'), this.handleError);
+    if (data.includes('{Msg S Param set}')) {
+      console.log('Param saved');
+      return;
     }
 
-    /**
-     * Command that we send to serial should have minimum 10 char
-     * Is because DMA buffer is processed every 10 chars
-     *
-     * If we have 22 char in command, we should send minimum 30
-     */
-    static alignCommand(command: string): string {
-        // @ts-ignore
-        return command.padEnd(30);
+    const parsedLine = QczekClass.parseParamLine(data);
+    if (parsedLine && parsedLine.version) {
+      this.onParams({ isMaster: parsedLine.version.startsWith('QczekLRS_M') });
     }
 
-    onData(data: string) {
-        if (!data) {
-            console.error('No data in response');
-            return;
-        }
+    return this.onParams(parsedLine);
+  }
 
-        console.log('data', data);
+  handleError = (error: Error) => {
+    if (error) {
+      this.onError(error.message);
+      console.error(error);
+    }
+  };
 
-        if (data.includes('{Msg S Param set}')) {
-            console.log('Param saved');
-            return;
-        }
+  static parseParamsFromFile(fileContent: string): IModuleParams {
+    const lines = fileContent.split('\n');
+    let params: IModuleParams = DEFAULT_MODULE_PARAMS;
 
-        const parsedLine = QczekClass.parseParamLine(data);
-        if (parsedLine && parsedLine.version) {
-            this.onParams({isMaster: parsedLine.version.startsWith('QczekLRS_M')});
-        }
+    lines.forEach(line => {
+      params = {
+        ...params,
+        ...QczekClass.parseParamLine(line),
+      };
+    });
 
-        return this.onParams(parsedLine);
+    return params;
+  }
+
+  static parseParamLine(line: string): Partial<IModuleParams> {
+    if (!line) {
+      return {};
     }
 
-    handleError = (error: Error) => {
-        if (error) {
-            this.onError(error.message);
-            console.error(error);
-        }
+    const param = line.match(/\{(ParSetStr|ParSetInt) (.*?) (.*?)\}/);
+    if (!param || param.length !== 4) {
+      console.error('Param parsing error', param);
+      return {};
     }
 
-    static parseParamsFromFile(fileContent: string): IModuleParams {
-        const lines = fileContent.split('\n');
-        let params: IModuleParams = DEFAULT_MODULE_PARAMS;
+    const paramNameShort = param[2];
+    let paramValue = !isNaN(parseInt(param[3])) ? parseInt(param[3]) : param[3];
 
-        lines.forEach((line) => {
-            params = {
-                ...params,
-                ...QczekClass.parseParamLine(line)
-            };
-        });
-
-        return params;
+    let paramName = paramNameShort;
+    if (paramNameShort === 'v.') {
+      paramName = 'version';
+    }
+    if (paramNameShort === 'f.') {
+      paramName = 'features';
+    }
+    // show frequency to user divided by 1000000
+    if (paramNameShort.startsWith('fr') || paramName === 'beaFreq') {
+      paramValue = +paramValue / 1000000;
+    }
+    // It’s set in tenths of second - show to user in seconds
+    if (paramNameShort === 'timeToFS') {
+      paramValue = +paramValue / 10;
     }
 
-    static parseParamLine(line: string): Partial<IModuleParams> {
-        if (!line) {
-            return {};
-        }
+    return { [paramName]: paramValue };
+  }
 
-        const param = line.match(/\{(ParSetStr|ParSetInt) (.*?) (.*?)\}/);
-        if (!param || param.length !== 4) {
-            console.error('Param parsing error', param);
-            return {};
-        }
+  static generateParamLine(key: string, value: string | number): string {
+    let paramSet = 'ParSetInt';
+    let paramName = key;
+    let paramValue = value;
 
-        const paramNameShort = param[2];
-        let paramValue = !isNaN(parseInt(param[3])) ? parseInt(param[3]) : param[3];
-
-        let paramName = paramNameShort;
-        if (paramNameShort === 'v.') {
-            paramName = 'version';
-        }
-        if (paramNameShort === 'f.') {
-            paramName = 'features';
-        }
-        // show frequency to user divided by 1000000
-        if (paramNameShort.startsWith('fr') || paramName === 'beaFreq') {
-            paramValue = +paramValue / 1000000;
-        }
-        // It’s set in tenths of second - show to user in seconds
-        if (paramNameShort === 'timeToFS') {
-            paramValue = +paramValue / 10;
-        }
-
-        return {[paramName]: paramValue};
+    if (key === 'version') {
+      paramSet = `ParSetStr`;
+      paramName = `v.`;
     }
 
-    static generateParamLine(key: string, value: string | number): string {
-        let paramSet = 'ParSetInt';
-        let paramName = key;
-        let paramValue = value;
-
-        if (key === 'version') {
-            paramSet = `ParSetStr`;
-            paramName = `v.`;
-        }
-
-        if (key === 'features') {
-            paramSet = `ParSetStr`;
-            paramName = `f.`;
-        }
-
-        if (paramName.startsWith('fr') || paramName === 'beaFreq') {
-            paramValue = +paramValue * 1000000;
-        }
-
-        if (paramName === 'timeToFS') {
-            paramValue = +paramValue * 10;
-        }
-
-        if (paramName === 'isMaster') {
-            return '';
-        }
-
-        return `{${paramSet} ${paramName} ${paramValue}}`;
+    if (key === 'features') {
+      paramSet = `ParSetStr`;
+      paramName = `f.`;
     }
+
+    if (paramName.startsWith('fr') || paramName === 'beaFreq') {
+      paramValue = +paramValue * 1000000;
+    }
+
+    if (paramName === 'timeToFS') {
+      paramValue = +paramValue * 10;
+    }
+
+    if (paramName === 'isMaster') {
+      return '';
+    }
+
+    return `{${paramSet} ${paramName} ${paramValue}}`;
+  }
 }
--- src/containers/SerialColumn.tsx
+++ src/containers/SerialColumn.tsx
@@ -1,4 +1,4 @@
-import {FormGroup, InputLabel, MenuItem} from '@material-ui/core';
+import { FormGroup, InputLabel, MenuItem } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
@@ -7,116 +7,158 @@ import Input from '@material-ui/core/Input';
 import Select from '@material-ui/core/Select';
 import Tooltip from '@material-ui/core/Tooltip';
 import * as React from 'react';
-import {IPortList} from '../components/SerialPortClass';
+import { IPortList } from '../components/SerialPortClass';
 
 interface IPortProps {
-    isPortOpened: boolean;
-    onConnectPortClick: (port: string) => void;
-    onDisconnectPortClick: () => void;
-    onReadParamsClick: () => void;
-    onSaveParamsClick: () => void;
-    onExportParamsClick: () => void;
-    onImportParamsClick: () => void;
-    onUpdatePortListClick: () => Promise<IPortList[]>;
+  isPortOpened: boolean;
+  onConnectPortClick: (port: string) => void;
+  onDisconnectPortClick: () => void;
+  onReadParamsClick: () => void;
+  onSaveParamsClick: () => void;
+  onExportParamsClick: () => void;
+  onImportParamsClick: () => void;
+  onUpdatePortListClick: () => Promise<IPortList[]>;
 }
 
 interface IPortState {
-    selectedPort: string;
-    portList: IPortList[];
+  selectedPort: string;
+  portList: IPortList[];
 }
 
 export default class SerialColumn extends React.Component<IPortProps, IPortState> {
-    state: IPortState = {
-        portList: [],
-        selectedPort: (localStorage.getItem('selectedPort')) as string
-    };
+  state: IPortState = {
+    portList: [],
+    selectedPort: localStorage.getItem('selectedPort') as string,
+  };
 
-    async componentDidMount() {
-        await this.updatePortList();
-    }
+  async componentDidMount() {
+    await this.updatePortList();
+  }
 
-    updatePortList = async () => {
-        const portList = await this.props.onUpdatePortListClick();
-        this.setState({portList});
-    }
+  updatePortList = async () => {
+    const portList = await this.props.onUpdatePortListClick();
+    this.setState({ portList });
+  };
 
-    render() {
-        return (
-            <div>
-                <FormGroup>
-                    <InputLabel htmlFor='serial-port'>Serial port</InputLabel>
-                    <Select
-                        title='Select serial port'
-                        value={this.state.selectedPort}
-                        onChange={(event: React.ChangeEvent<{ name?: string; value: string }>) => {
-                            this.setState({selectedPort: event.target.value});
-                            localStorage.setItem('selectedPort', event.target.value);
-                        }}
-                        input={<Input name='serial-port' id='serial-port'/>}
-                    >
-                        <MenuItem value=''>
-                            <em>Select serial port</em>
-                        </MenuItem>
-                        {this.state.portList.map((port, i) => {
-                            return <MenuItem key={i} value={port.comName}>{port.comName}</MenuItem>;
-                        })}
-                    </Select>
-                </FormGroup>
+  render() {
+    return (
+      <div>
+        <FormGroup>
+          <InputLabel htmlFor="serial-port">Serial port</InputLabel>
+          <Select
+            title="Select serial port"
+            value={this.state.selectedPort}
+            onChange={(event: React.ChangeEvent<{ name?: string; value: string }>) => {
+              this.setState({ selectedPort: event.target.value });
+              localStorage.setItem('selectedPort', event.target.value);
+            }}
+            input={<Input name="serial-port" id="serial-port" />}
+          >
+            <MenuItem value="">
+              <em>Select serial port</em>
+            </MenuItem>
+            {this.state.portList.map((port, i) => {
+              return (
+                <MenuItem key={i} value={port.comName}>
+                  {port.comName}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormGroup>
 
-                <Divider style={{margin: '10px 0'}}/>
+        <Divider style={{ margin: '10px 0' }} />
 
-                <Grid container spacing={2}>
-                    <Grid item xs={6} sm={6}>
-                        <Tooltip title='Update serial port list'>
-                            <Button variant='contained' fullWidth={true} size='small' title='' onClick={this.updatePortList}>
-                                Update <Icon>autorenew</Icon>
-                            </Button>
-                        </Tooltip>
-                    </Grid>
-                    <Grid item xs={6} sm={6}>
-                        {this.props.isPortOpened ?
-                            <Button variant='contained' color='secondary' fullWidth={true} size='small'
-                                    onClick={this.props.onDisconnectPortClick}>
-                                Disconnect <Icon>phonelink_erase</Icon>
-                            </Button>
-                            :
-                            <Button variant='contained' color='primary' fullWidth={true} size='small'
-                                    onClick={() => this.props.onConnectPortClick(this.state.selectedPort)}>
-                                Connect <Icon>phonelink_ring</Icon>
-                            </Button>
-                        }
-                    </Grid>
-                </Grid>
+        <Grid container spacing={2}>
+          <Grid item xs={6} sm={6}>
+            <Tooltip title="Update serial port list">
+              <Button
+                variant="contained"
+                fullWidth={true}
+                size="small"
+                title=""
+                onClick={this.updatePortList}
+              >
+                Update <Icon>autorenew</Icon>
+              </Button>
+            </Tooltip>
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            {this.props.isPortOpened ? (
+              <Button
+                variant="contained"
+                color="secondary"
+                fullWidth={true}
+                size="small"
+                onClick={this.props.onDisconnectPortClick}
+              >
+                Disconnect <Icon>phonelink_erase</Icon>
+              </Button>
+            ) : (
+              <Button
+                variant="contained"
+                color="primary"
+                fullWidth={true}
+                size="small"
+                onClick={() => this.props.onConnectPortClick(this.state.selectedPort)}
+              >
+                Connect <Icon>phonelink_ring</Icon>
+              </Button>
+            )}
+          </Grid>
+        </Grid>
 
-                <Divider style={{margin: '10px 0'}}/>
+        <Divider style={{ margin: '10px 0' }} />
 
-                <Grid container spacing={2}>
-                    <Grid item xs={6} sm={6}>
-                        <Button variant='contained' fullWidth={true} size='small' onClick={this.props.onReadParamsClick}>
-                            Read params
-                        </Button>
-                    </Grid>
-                    <Grid item xs={6} sm={6}>
-                        <Button variant='contained' color='primary' fullWidth={true} size='small' onClick={this.props.onSaveParamsClick}>
-                            Save params
-                        </Button>
-                    </Grid>
-                </Grid>
-                <Divider style={{margin: '10px 0'}}/>
+        <Grid container spacing={2}>
+          <Grid item xs={6} sm={6}>
+            <Button
+              variant="contained"
+              fullWidth={true}
+              size="small"
+              onClick={this.props.onReadParamsClick}
+            >
+              Read params
+            </Button>
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth={true}
+              size="small"
+              onClick={this.props.onSaveParamsClick}
+            >
+              Save params
+            </Button>
+          </Grid>
+        </Grid>
+        <Divider style={{ margin: '10px 0' }} />
 
-                <Grid container spacing={2}>
-                    <Grid item xs={6} sm={6}>
-                        <Button variant='contained' fullWidth={true} size='small' onClick={this.props.onExportParamsClick}>
-                            Export params
-                        </Button>
-                    </Grid>
-                    <Grid item xs={6} sm={6}>
-                        <Button variant='contained' color='primary' fullWidth={true} size='small' onClick={this.props.onImportParamsClick}>
-                            Import params
-                        </Button>
-                    </Grid>
-                </Grid>
-            </div>
-        );
-    }
+        <Grid container spacing={2}>
+          <Grid item xs={6} sm={6}>
+            <Button
+              variant="contained"
+              fullWidth={true}
+              size="small"
+              onClick={this.props.onExportParamsClick}
+            >
+              Export params
+            </Button>
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth={true}
+              size="small"
+              onClick={this.props.onImportParamsClick}
+            >
+              Import params
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
