9b08505de9f002707fda32e4c03a785ce738b623
mosvov <vmoskalyk@goodrx.com>
2021-10-12 21:25:57 -0400

    Github Workflow

--- .forge.config.js
+++ .forge.config.js
@@ -1,39 +1,59 @@
-const arch = require('os').arch();
 module.exports = {
-    "make_targets": {
-        "win32": [
-            "squirrel"
+    packagerConfig: {
+        packageManager: 'yarn',
+        icon: './src/icons/serial_config',
+        dir: './src',
+    },
+    makers: [
+        {
+            name: '@electron-forge/maker-dmg',
+            config: {
+                icon: './src/icons/serial_config.icns',
+            },
+        },
+        {
+            name: '@electron-forge/maker-squirrel',
+            config: {
+                name: 'qczek_modules_setting',
+            },
+        },
+        {
+            name: '@electron-forge/maker-deb',
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-rpm',
+            config: {},
+        },
+    ],
+    publishes: [
+      {
+        name: '@electron-forge/publisher-github',
+        config: {
+          repository: {
+            owner: 'mosvov',
+            name: 'qczek-modules-setting',
+          },
+          prerelease: true,
+        },
+      },
+    ],
+    plugins: [
+        [
+            '@electron-forge/plugin-webpack',
+            {
+                mainConfig: './webpack/webpack.main.config.js',
+                renderer: {
+                    config: './webpack/webpack.renderer.config.js',
+                    entryPoints: [
+                        {
+                            html: './src/index.html',
+                            js: './src/renderer.tsx',
+                            name: 'main_window',
+                        },
+                    ],
+                },
+            },
         ],
-        "darwin": [
-            "dmg"
-        ],
-        "linux": [
-            "deb",
-            "rpm"
-        ]
-    },
-    "electronPackagerConfig": {
-        "packageManager": "yarn",
-        "icon": "./src/icons/serial_config",
-        "dir": "./src"
-    },
-    "electronWinstallerConfig": {
-        "name": `qczek_modules_setting_${arch}`
-    },
-    "electronInstallerDMG": {
-        "icon": "./src/icons/serial_config.icns"
-    },
-    "electronInstallerDebian": {},
-    "electronInstallerRedhat": {},
-    "github_repository": {
-        "owner": "mosvov",
-        "name": "qczek-modules-setting",
-        "options": {
-            "host": 'api.github.com',
-        }
-    },
-    "windowsStoreConfig": {
-        "packageName": "",
-        "name": "qczekmodulessetting"
-    }
-}
+    ],
+};
